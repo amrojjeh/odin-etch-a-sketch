78275e5b5d157261f293f65a6b6f3ef30c68fa9b
Amr Ojjeh <amrojjeh@outlook.com>
2022-10-30 21:37:55 -0500

    Add paint

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ function generateGrid() {
 	for (let x = 0; x < 16; ++x) {
 		for (let y = 0; y < 16; ++y) {
 			let newDiv = document.createElement("div");
-			newDiv.classList.add("square");
+			newDiv.classList.add("square", "red");
+			newDiv.addEventListener("mouseenter", paint);
 			divContainer.appendChild(newDiv);
 		}
 	}
+}
+
+function paint(e) {
+	e.target.classList.remove("red");
+	e.target.classList.add("blue");
 }
 
 document.body.onload = generateGrid;
