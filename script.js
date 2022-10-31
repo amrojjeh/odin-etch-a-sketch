b5f766a0bbe8051a54e624d724c5ed5d7156fe84
Amr Ojjeh <amrojjeh@outlook.com>
2022-10-30 22:54:21 -0500

    Add new board logic

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-// Generates the 16x16 grid
-function generateGrid() {
+// Generates a 720px by 720px grid
+function generateGrid(from_form) {
+	let size = 16; // default size
+	if (from_form) {
+		let inputElement = document.querySelector("#size");
+		size = parseInt(inputElement.value); // TODO: check error
+		inputElement.setAttribute("placeholder", size);
+	}
 	let divContainer = document.querySelector("#board");
-	for (let x = 0; x < 16; ++x) {
-		for (let y = 0; y < 16; ++y) {
+	divContainer.innerHTML = "";
+	for (let x = 0; x < size; ++x) {
+		for (let y = 0; y < size; ++y) {
 			let newDiv = document.createElement("div");
 			newDiv.classList.add("square", "red");
 			newDiv.addEventListener("mouseenter", paint);
 			divContainer.appendChild(newDiv);
+
+			let pixels_per_square = 720 / size;
+			divContainer.style.gridTemplateColumns = `repeat(${size}, ${pixels_per_square}px)`;
+			divContainer.style.gridTemplateRows = `repeat(${size}, ${pixels_per_square}px)`;
 		}
 	}
 }
@@ -16,4 +27,4 @@ function paint(e) {
 	e.target.classList.add("blue");
 }
 
-document.body.onload = generateGrid;
+document.body.onload = () => generateGrid(false);
